6297014503993c24c71bb0b781225f36e4bc4804
Peterson Macedo <petersonmacedo.pm@gmail.com>
2023-07-16 20:20:11 -0400

    Adding Items to the List

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -19,64 +19,95 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final _toDoController = TextEditingController();
+
   List _toDolist = [];
+
+  void _addTodo(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDolist.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Lista de Tarefas"),
-        backgroundColor: Colors.pink,
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-            child:  Row(
-              children: <Widget>[
-                const Expanded(
+        appBar: AppBar(
+          title: const Text("Lista de Tarefas"),
+          backgroundColor: Colors.pink,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: <Widget>[
+                 Expanded(
                     child: TextField(
+                      controller: _toDoController,
                       decoration: InputDecoration(
                         labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(color: Colors.pink ),
+                        labelStyle: TextStyle(color: Colors.pink),
                       ),
                     ),),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pink,
-                  ),
-                  child: Text("ADD"),
-                  onPressed: () {},
-                )
-              ],
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                    ),
+                    child: Text("ADD"),
+                    onPressed: _addTodo,
+                  )
+                ],
+              ),
             ),
-          )
-        ],
-      )
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 10.0),
+                itemCount: _toDolist.length,
+                itemBuilder: (context, int index) {
+                  return CheckboxListTile(
+                    title:Text (_toDolist[index] ["title"]),
+                    value: _toDolist[index] ["ok"],
+                    secondary: CircleAvatar(child: Icon(_toDolist[index] ["ok"] ?
+                    Icons.check : Icons.error),),
+                    onChanged: (c) {
+                      setState(() {
+                        _toDolist[index]["ok"] = c;
+                      });
+                    } ,
+                  );
+                },
+              ),
+            ),
+          ],
+        )
     );
   }
 
-  Future<File> _getFile() async{
+  Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
 
-  Future<File> _saveData() async{
+  Future<File> _saveData() async {
     String data = json.encode(_toDolist);
     final file = await _getFile();
     return file.writeAsString(data);
-
   }
-  Future<String> _readData() async{
-    try{
-        final file = await _getFile();
-        return file.readAsString();
-      }catch (e){
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
       print('Error reading data: $e');
       return '';
-    }finally{
-    }
+    } finally {}
   }
 }
 
